8519e1099f11b5c1e576c04cbcbe5189b5524b07
Felix Schwarz <87583174+FatGarfield777@users.noreply.github.com>
2025-03-01 14:00:41 -0500

    and im totally original

--- proxy/uv/uv.config.js
+++ proxy/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/active/go/',
+    prefix: '/proxy',
     bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
